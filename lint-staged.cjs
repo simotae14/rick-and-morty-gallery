d636e2bf90f5f9828736c5cfeab06c0e01ec8621
Simone Taeggi <startae14@gmail.com>
2023-05-13 16:29:07 +0200

    fix pre commit

--- lint-staged.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'src/**/*.{js,jsx,ts,tsx}': [
-      'npx prettier --write',
-      'eslint',
-      'vitest --passWithNoTests',
-  ],
-  '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
-}
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
-const unused2 = true;
+const unused = true;
 
 export function App() {
   return (
