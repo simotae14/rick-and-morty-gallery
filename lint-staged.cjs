1c6e0f27ef8936150858c165528ef4989c58f3f5
Simone Taeggi <startae14@gmail.com>
2023-05-13 11:26:56 +0200

    change pre-commit steps

--- lint-staged.cjs
+++ lint-staged.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': [
-      'eslint --max-warnings=0', 
-      'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
+      'eslint --max-warnings=0',
+      'npx prettier',
+      'vitest --bail --watchAll=false --findRelatedTests --passWithNoTests',
       () => 'tsc-files --noEmit',
   ],
   '*.{js,jsx,ts,tsx,json,css,js}': ['prettier --write'],
