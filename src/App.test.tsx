e93f58f4c91d51199cfac9c24bbc0df2cad56ee9
Simone Taeggi <startae14@gmail.com>
2023-05-13 02:12:58 +0200

    add another unit test on the navigation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,22 @@
 import { describe, it } from 'vitest';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter, BrowserRouter } from 'react-router-dom';
 
-import App from './App';
+import { App } from './App';
+
+function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
 
 describe('App', () => {
   it('Renders hello world', () => {
     // ARRANGE
-    render(<App />);
+    render(<WrappedApp />);
     // ACT
     // EXPECT
     expect(
@@ -15,5 +24,17 @@ describe('App', () => {
         level: 1,
       })
     ).toHaveTextContent('Hello World');
+  });
+  it('Renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/this-route-does-not-exist']}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(
+      screen.getByRole('heading', {
+        level: 1,
+      })
+    ).toHaveTextContent('Not Found');
   });
 });
