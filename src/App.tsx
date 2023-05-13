6206709a7b8a830f0b0c4311c93ebf27aa7fe0d6
Simone Taeggi <startae14@gmail.com>
2023-05-13 16:49:35 +0200

    bundle size reporter

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
-const unudes = true;
-
 export function App() {
   return (
     <Routes>
