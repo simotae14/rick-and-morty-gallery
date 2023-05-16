04b8d37ce061b67d5e0d06b2056be5d7db88a8b9
Simone Taeggi <startae14@gmail.com>
2023-05-16 16:44:47 +0200

    Add Suspense and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
 import Layout from './components/molecules/Layout';
 import Loading from './pages/loading';
 import { NotFoundText, Container } from './App.styles';
 import ErrorBoundary from './components/organisms/ErrorBoundary';
 
+const Home = lazy(() => import('./pages/home'));
+
 export function App() {
   return (
-    <Layout>
-      <ErrorBoundary>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="*"
-            element={
-              <Container>
-                <Loading />
-                <NotFoundText>Page Not Found! Click on the title</NotFoundText>
-              </Container>
-            }
-          />
-        </Routes>
-      </ErrorBoundary>
-    </Layout>
+    <Suspense fallback={<Loading />}>
+      <Layout>
+        <ErrorBoundary>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="*"
+              element={
+                <Container>
+                  <Loading />
+                  <NotFoundText>
+                    Page Not Found! Click on the title
+                  </NotFoundText>
+                </Container>
+              }
+            />
+          </Routes>
+        </ErrorBoundary>
+      </Layout>
+    </Suspense>
   );
 }
