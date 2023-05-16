2a774d277302de99fa35255851567a60ab20f633
Simone Taeggi <startae14@gmail.com>
2023-05-16 23:27:07 +0200

    added some e2e tests

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -1,8 +1,25 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+test.describe('Show the Gallery', () => {
+  test('should show the title on first rendering', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('Rick and Morty Gallery');
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle('Rick and Morty Gallery');
+  });
+
+  test('should allow me to change page', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+
+    await page.locator('ul li:nth-child(2)').click();
+  });
+
+  test('should allow me to select a character', async ({ page }) => {
+    await page.goto('http://localhost:5173/');
+
+    await page.locator('text=Rick Sanchez').click();
+
+    // Expect the details page.
+    await expect(page).toHaveURL('http://localhost:5173/details');
+  });
 });
