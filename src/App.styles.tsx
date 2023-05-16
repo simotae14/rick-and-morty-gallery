284eb6760bcbb9fef5c0ea68bbe40ff68b807983
Simone Taeggi <startae14@gmail.com>
2023-05-16 14:46:21 +0200

    fix fonts

---
+++ src/App.styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
+
+export const NotFoundText = styled.h2`
+  font-size: 2rem;
+  font-weight: 800;
+  text-align: center;
+  margin-top: 0;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
 import Layout from './components/molecules/Layout';
+import Loading from './pages/loading';
+import { NotFoundText, Container } from './App.styles';
 
 export function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="*"
+          element={
+            <Container>
+              <Loading />
+              <NotFoundText>Page Not Found! Click on the title</NotFoundText>
+            </Container>
+          }
+        />
       </Routes>
     </Layout>
   );
